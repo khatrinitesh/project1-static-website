d7aa94cc08555aa9d61e3d399286a9a33e85f523
nitesh <nitesh.khatri88@gmail.com>
2023-11-22 23:31:16 +0530

    added section services, feature, banner and slideshow

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
     'use strict';
+
+    // START SECTION >> BANNER
+    $(function(){
+        $('#bannerSlideshow').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            adaptiveHeight: true
+          });
+    });
+    // END SECTION >> BANNER
 });

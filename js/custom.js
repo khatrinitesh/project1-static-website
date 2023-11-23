cfcb148ccbdde853ccf866bafae966595bec109a
nitesh <nitesh.khatri88@gmail.com>
2023-11-23 09:12:03 +0530

    added feature code section slideshow and every section responsive all devices

--- js/custom.js
+++ js/custom.js
@@ -9,8 +9,40 @@ $(document).ready(function(){
             speed: 300,
             slidesToShow: 1,
             adaptiveHeight: true,
-            arrows: true
-          });
+            arrows: true,
+            autoplaySpeed: 5000,
+            autoplay: true
+        });
     });
     // END SECTION >> BANNER
+
+    // START SECTION >> CUSTOMER TESTIMONIALS
+    $(function(){
+        $('#slideshowTestimonials').slick({
+            dots: false,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 2,
+            adaptiveHeight: true,
+            arrows: true,
+            autoplaySpeed: 5000,
+            autoplay: true,
+            responsive: [
+                { 
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+                { 
+                    breakpoint: 776,
+                    settings: {
+                        slidesToShow: 1,
+                        arrow:false,
+                        dots:true
+                    }
+            }]
+        });
+    });
+    // END SECTION >> CUSTOMER TESTIMONIALS
 });

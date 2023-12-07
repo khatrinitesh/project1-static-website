7536f3c8c7e10a3563341d4eee528039f4ea1be8
nitesh <nitesh.khatri88@gmail.com>
2023-12-07 23:46:46 +0530

    added detail page for all devices

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,78 @@
 $(document).ready(function(){
     'use strict';
 
+    // START SECTION >> TRENDING SLIDER
+    $(function(){
+        var swiper = new Swiper(".trending_slider", {
+            slidesPerView: 3,
+            spaceBetween: 30,
+            loop: true,
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+            breakpoints: {
+                320: {
+                  slidesPerView: 1.5
+                },
+                991: {
+                  slidesPerView: 2
+                },
+                992: {
+                  slidesPerView: 3
+                }
+              }
+        });
+    });
+
+    $(function(){
+                const sliderThumbs = new Swiper('.slider__thumbs .swiper-container', { // ищем слайдер превью по селектору
+                    // задаем параметры
+                    direction: 'vertical', // вертикальная прокрутка
+                    slidesPerView: 3, // показывать по 3 превью
+                    spaceBetween: 24, // расстояние между слайдами
+                    navigation: { // задаем кнопки навигации
+                        nextEl: '.slider__next', // кнопка Next
+                        prevEl: '.slider__prev' // кнопка Prev
+                    },
+                    freeMode: true, // при перетаскивании превью ведет себя как при скролле
+                    breakpoints: { // условия для разных размеров окна браузера
+                        0: { // при 0px и выше
+                            direction: 'horizontal', // горизонтальная прокрутка
+                        },
+                        768: { // при 768px и выше
+                            direction: 'vertical', // вертикальная прокрутка
+                        }
+                    }
+                });
+                // Инициализация слайдера изображений
+                const sliderImages = new Swiper('.slider__images .swiper-container', { // ищем слайдер превью по селектору
+                    // задаем параметры
+                    direction: 'vertical', // вертикальная прокрутка
+                    slidesPerView: 1, // показывать по 1 изображению
+                    spaceBetween: 32, // расстояние между слайдами
+                    mousewheel: true, // можно прокручивать изображения колёсиком мыши
+                    navigation: { // задаем кнопки навигации
+                        nextEl: '.slider__next', // кнопка Next
+                        prevEl: '.slider__prev' // кнопка Prev
+                    },
+                    grabCursor: true, // менять иконку курсора
+                    thumbs: { // указываем на превью слайдер
+                        swiper: sliderThumbs // указываем имя превью слайдера
+                    },
+                    breakpoints: { // условия для разных размеров окна браузера
+                        0: { // при 0px и выше
+                            direction: 'horizontal', // горизонтальная прокрутка
+                        },
+                        768: { // при 768px и выше
+                            direction: 'vertical', // вертикальная прокрутка
+                        }
+                    }
+                });
+            });
+
+    // END SECTION >> TRENDING SLIDER
+
     // START SECTION >> BANNER
     $(function(){
         $('#bannerSlideshow').slick({
